3b79f9f73478732a20d9f6f52c29c7d32b622be5
affli <delycid@gmail.com>
2010-10-22 01:29:32 +0400

    a typo.

--- Tukui/modules/temp.lua
+++ Tukui/modules/temp.lua
@@ -5,7 +5,7 @@ local TooltipHider = CreateFrame("Frame")
 TooltipHider:RegisterEvent("PLAYER_ENTERING_WORLD")
 TooltipHider:SetScript("OnEvent", function()
 	local inInstance, instanceType = IsInInstance()
-	if inInstance and (instanceType == "party" or instanceType == "pvp"or InstanceType == "raid") then
+	if inInstance and (instanceType == "party" or instanceType == "pvp"or instanceType == "raid") then
 		print("|cffC495DDElvUI|r: One-Lined tooltips temporarily disabled while inside instance to prevent client crashes")
 		GameTooltip:SetScript("OnShow", function()
 			if GameTooltip:NumLines() == 1 then
